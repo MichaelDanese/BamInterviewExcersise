749c30495517c1dca3f573ff85e101683e93b41a
Michael Danese <JoshDanese@gmail.com>
2026-02-24 17:21:52 -0500

    Updated Stargate Log to Include Details

--- Business/Data/StargateLog.cs
+++ Business/Data/StargateLog.cs
@@ -10,6 +10,7 @@ namespace StargateAPI.Business.Data
         public int StargateLogId { get; set; }
         public int StargateLogSeverityId { get; set; }
         public string Message { get; set; } = string.Empty;
+        public string Details { get; set; } = string.Empty;
         public string? Exception { get; set; }
         public DateTime CreatedOn {  get; set; }
 
@@ -24,6 +25,7 @@ namespace StargateAPI.Business.Data
             builder.HasKey(x => x.StargateLogId);
             builder.Property(x => x.StargateLogId).ValueGeneratedOnAdd();
             builder.Property(x => x.Message).IsRequired();
+            builder.Property(x => x.Details).IsRequired();
             builder.HasIndex(x => x.CreatedOn);
 
         }
