8e1118ba3c2861fa6ecb95d5a248dbe4dede37f3
Michael Danese <JoshDanese@gmail.com>
2026-02-24 17:22:38 -0500

    Added Interface for Logging

---
+++ Services/Interfaces/ILoggingService.cs
@@ -0,0 +1,12 @@
+﻿using StargateAPI.Enums;
+
+namespace StargateAPI.Services.Interfaces
+{
+    public interface ILoggingService
+    {
+        Task LogAsync(StargateLogSeverityEnum severity, string message, string details, Exception? exception);
+        Task LogInfoAsync(string message, string details);
+        Task LogWarningAsync(string message, string details);
+        Task LogErrorAsync(string message, string details,  Exception? exception);
+    }
+}
