01dcbca2402da64c7a73bbf9c4517b4cb2b47d7c
Michael Danese <JoshDanese@gmail.com>
2026-02-25 18:07:32 -0500

    Added Log and README

--- Backend/StargateAPI/Business/Commands/CreatePerson.cs
+++ Backend/StargateAPI/Business/Commands/CreatePerson.cs
@@ -3,6 +3,7 @@ using MediatR.Pipeline;
 using Microsoft.EntityFrameworkCore;
 using StargateAPI.Business.Data;
 using StargateAPI.Business.Extensions;
+using StargateAPI.Business.Services.Interfaces;
 using StargateAPI.Controllers;
 
 namespace StargateAPI.Business.Commands
@@ -15,10 +16,12 @@ namespace StargateAPI.Business.Commands
     public class CreatePersonPreProcessor : IRequestPreProcessor<CreatePerson>
     {
         private readonly StarbaseContext _context;
+        
         public CreatePersonPreProcessor(StarbaseContext context)
         {
             _context = context;
         }
+
         public async Task Process(CreatePerson request, CancellationToken cancellationToken)
         {
             if (string.IsNullOrWhiteSpace(request.Name))
@@ -41,10 +44,12 @@ namespace StargateAPI.Business.Commands
     public class CreatePersonHandler : IRequestHandler<CreatePerson, CreatePersonResult>
     {
         private readonly StarbaseContext _context;
+        private readonly IDatabaseLoggingService _logger;
 
-        public CreatePersonHandler(StarbaseContext context)
+        public CreatePersonHandler(StarbaseContext context, IDatabaseLoggingService logger)
         {
             _context = context;
+            _logger = logger;
         }
         public async Task<CreatePersonResult> Handle(CreatePerson request, CancellationToken cancellationToken)
         {
@@ -58,6 +63,12 @@ namespace StargateAPI.Business.Commands
             await _context.People.AddAsync(newPerson, cancellationToken);
 
             await _context.SaveChangesAsync(cancellationToken);
+
+            await _logger.LogInfoAsync(
+                "Successfully created person",
+                $"Successfully created person with name {normalizedName}"
+            );
+
             return new CreatePersonResult()
             {
                 Id = newPerson.Id,
