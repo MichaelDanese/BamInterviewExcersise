883ace5b568197d90f9d20fadd34bf9ed6d3ad7e
Michael Danese <JoshDanese@gmail.com>
2026-02-25 19:11:24 -0500

    Added Environment information

--- Frontend/stargate-frontend/src/app/services/astronaut.service.ts
+++ Frontend/stargate-frontend/src/app/services/astronaut.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 import { BaseResponse, CreateAstronautDutyRequest, GetAstronautDutiesByNameResult } from '../models';
 
 @Injectable({ providedIn: 'root' })
 export class AstronautService {
+  private readonly apiBaseUrl = (environment.apiBaseUrl ?? '').replace(/\/$/, '');
+
   constructor(private readonly http: HttpClient) {}
 
   getAstronautDutiesByName(name: string): Observable<GetAstronautDutiesByNameResult> {
-    return this.http.get<GetAstronautDutiesByNameResult>(`/AstronautDuty/${encodeURIComponent(name)}`);
+    return this.http.get<GetAstronautDutiesByNameResult>(`${this.apiBaseUrl}/AstronautDuty/${encodeURIComponent(name)}`);
   }
 
   createAstronautDuty(payload: CreateAstronautDutyRequest): Observable<BaseResponse> {
-    return this.http.post<BaseResponse>('/AstronautDuty', payload);
+    return this.http.post<BaseResponse>(`${this.apiBaseUrl}/AstronautDuty`, payload);
   }
 }
 
